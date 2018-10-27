9578d5fd770be5c7749afde7082a765f190ff26b
kawashima41 <i.kawashima41@gmail.com>
2018-10-28 00:03:04 +0900

    add Button components

--- Sakunomi/SignUpViewController.swift
+++ Sakunomi/SignUpViewController.swift
@@ -4,6 +4,11 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailLabel: UITextField!
+    
+    @IBOutlet weak var passwordLabel: UITextField!
+    @IBOutlet weak var personalinformation: TextButton!
+    @IBOutlet weak var createAccountButton: TextButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
