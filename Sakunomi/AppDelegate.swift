e56bc67e2b45b4b74c9ce7a91ed1f4071ef13510
kawashima41 <i.kawashima41@gmail.com>
2018-11-04 06:04:56 +0900

    Add support for launching AppRouteVC

--- Sakunomi/AppDelegate.swift
+++ Sakunomi/AppDelegate.swift
@@ -13,9 +13,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    static var shared: AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+    var rootViewController: AppRootViewController {
+        return window?.rootViewController as! AppRootViewController
+    }
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = AppRootViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
