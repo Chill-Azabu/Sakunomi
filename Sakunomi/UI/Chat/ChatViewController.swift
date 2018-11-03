c2ee47094576c00292d0db636c0bb66f8dda73bb
kawashima41 <i.kawashima41@gmail.com>
2018-11-04 03:54:00 +0900

    Make the chat function

--- Sakunomi/UI/Chat/ChatViewController.swift
+++ Sakunomi/UI/Chat/ChatViewController.swift
@@ -1,9 +1,123 @@
 
 import UIKit
+import MessageKit
 
-class ChatViewController: UIViewController {
 
+// TODO: Move to Model Group
+struct Member {
+    let name: String
+    let color: UIColor
+}
+
+struct Message {
+    let member: Member
+    let text: String
+    let messageId: String
+}
+
+extension Message: MessageType {
+    var sender: Sender {
+        return Sender(id: member.name, displayName: member.name)
+    }
+    
+    var sentDate: Date {
+        return Date()
+    }
+    
+    var kind: MessageKind {
+        return .text(text)
+    }
+}
+
+extension Member {
+    var toJSON: Any {
+        return [
+            "name": name,
+            "color": color.hexString
+        ]
+    }
+    
+    init?(fromJSON json: Any) {
+        guard
+            let data = json as? [String: Any],
+            let name = data["name"] as? String,
+            let hexColor = data["color"] as? String
+            else {
+                print("Couldn't parse Member")
+                return nil
+        }
+        
+        self.name = name
+        self.color = UIColor(hex: hexColor)
+    }
+}
+
+class ChatViewController: MessagesViewController {
+    
+    var messages: [Message] = []
+    var member: Member!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        member = Member(name: "bluemoon", color: .blue)
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messageInputBar.delegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+}
+
+extension ChatViewController: MessagesDataSource {
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+    func currentSender() -> Sender {
+        return Sender(id: member.name, displayName: member.name)
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func messageTopLabelHeight(
+        for message: MessageType,
+        at indexPath: IndexPath,
+        in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        
+        return 12
+    }
+    
+    func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        
+        return NSAttributedString(string: message.sender.displayName, attributes: [.font: UIFont.systemFont(ofSize: 12)])
+    }
+}
+
+extension ChatViewController: MessagesLayoutDelegate {
+    func heightForLocation(message: MessageType, at indexPath: IndexPath, with maxWidth: CGFloat, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 0
+    }
+}
+
+// MARK :- AvatarView
+extension ChatViewController: MessagesDisplayDelegate {
+    func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+        
+        let message = messages[indexPath.section]
+        let color = message.member.color
+        avatarView.backgroundColor = color
+    }
+}
+
+extension ChatViewController: MessageInputBarDelegate {
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        let newMessage = Message(member: member, text: text, messageId: UUID().uuidString)
+        
+        messages.append(newMessage)
+        inputBar.inputTextView.text = ""
+        messagesCollectionView.reloadData()
+        messagesCollectionView.scrollToBottom(animated: true)
     }
 }
